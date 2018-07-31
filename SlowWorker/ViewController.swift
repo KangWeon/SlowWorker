d4cd61b18b6659077bed6bde73911575efacf0e3
klarheit <klarheit@klarheits-macbook-pro.local>
2018-07-31 20:28:56 +0900

    Initial Commit

---
+++ SlowWorker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlowWorker
+//
+//  Created by klarheit on 31/07/2018.
+//  Copyright © 2018 klarheit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
